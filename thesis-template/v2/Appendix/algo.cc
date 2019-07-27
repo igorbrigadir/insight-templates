eba05c3b91f6827f29d215b6ef838223f96e6791
Igor Brigadir <igor.brigadir@gmail.com>
2019-07-27 14:53:27 +0100

    remove unused thesis templates

--- thesis-template/v2/Appendix/algo.cc
+++
@@ -1,8 +0,0 @@
-/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
-
-#include <math.h>
-
-// this function returns a random value between 0 and 1 as double
-double GetRandVar (void)	{
-	return double(std::rand()) / double(RAND_MAX);
-}
